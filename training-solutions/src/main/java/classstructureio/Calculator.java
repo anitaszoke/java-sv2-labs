6c1f9c1ae18a8c11d0db591411b46c8060751054
anitaszoke <szanita1735@gmail.com>
2021-10-18 20:32:30 +0200

    Calculator task done, második scanner sor törölve

--- training-solutions/src/main/java/classstructureio/Calculator.java
+++ training-solutions/src/main/java/classstructureio/Calculator.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
 
         System.out.println("Add an integer: ");
-        Scanner scanner = new Scanner (System.in);
         int integer = scanner.nextInt();
 
         System.out.println("Add another integer: ");
-        Scanner scannerAnother = new Scanner (System.in);
         int integerAnother = scanner.nextInt();
 
         System.out.println(integer+" + "+integerAnother);
