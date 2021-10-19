5ee3614a469903df03b9f53a2a8c7398f47f429c
anitaszoke <szanita1735@gmail.com>
2021-10-19 21:54:56 +0200

    Bankaccount task

---
+++ training-solutions/src/main/java/classstructureintegrate/Bank.java
@@ -0,0 +1,17 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Account number?");
+        String accountNumber = scanner.nextLine();
+        
+
+    }
+
+
+}
