423677adaf427e3a3e0cfc5a19acd78fdc16f16a
anitaszoke <szanita1735@gmail.com>
2021-10-25 15:41:23 +0200

    Tanulócsoportok StudyGroup task done

---
+++ training-solutions/src/main/java/looptypes/StudyGroup.java
@@ -0,0 +1,26 @@
+package looptypes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StudyGroup {
+
+    public void printStudyGroups(List<String> students) {
+        for (String item : students) {
+            if (item.length() <= 10) {
+                System.out.println("1-es csoport: " + item);
+            } else {
+                System.out.println("2-es csoport: " + item);
+
+            }
+        }
+      }
+
+    public static void main(String[] args) {
+        List<String> students = Arrays.asList("Cserepes Virág","Kukor Ica", "Harry Potter", "Alma Málna", "Kis Peti");
+        StudyGroup studyGroup = new StudyGroup();
+        studyGroup.printStudyGroups(students);
+        
+
+    }
+}
