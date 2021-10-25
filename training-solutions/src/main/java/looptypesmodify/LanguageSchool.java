7237c55d784061fcf747c73caccc4d6855ace864
anitaszoke <szanita1735@gmail.com>
2021-10-25 20:16:44 +0200

    Nyelviskola elkezdve

---
+++ training-solutions/src/main/java/looptypesmodify/LanguageSchool.java
@@ -0,0 +1,39 @@
+package looptypesmodify;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageSchool {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+
+        Student student1 = new Student("Géza");
+        Student student2 = new Student("Kati");
+        Student student3 = new Student("Szabi");
+        Student student4 = new Student("Viki");
+        Student student5 = new Student("Béla");
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+        student1.setActive(false);
+        student2.setActive(false);
+
+        List<Student> removeStudent = new ArrayList<>();
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
