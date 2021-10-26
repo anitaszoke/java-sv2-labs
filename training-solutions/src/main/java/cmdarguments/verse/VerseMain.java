de6286afa569cc89c5bc4d0e57e00273fb48fa2e
anitaszoke <szanita1735@gmail.com>
2021-10-26 21:22:28 +0200

    Verssor összefűzése szavakból task done

--- training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
+++ training-solutions/src/main/java/cmdarguments/verse/VerseMain.java
@@ -1,4 +1,20 @@
 package cmdarguments.verse;
 
 public class VerseMain {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+
+        for (String k : args) {
+            System.out.print(k+" ");
+        }
+
+        System.out.println();
+
+        for (int i = args.length-1; i >=0; i--){
+            System.out.print(args[i] + " ");
+        }
+    }
 }
