c0d4c63c9961a70454fda0751b58249a0e367167
anitaszoke <szanita1735@gmail.com>
2021-10-26 18:53:11 +0200

    Kerület, terület, felszín és térfogat task done

---
+++ training-solutions/src/main/java/finalmodifier/CircleCalculator.java
@@ -0,0 +1,18 @@
+package finalmodifier;
+
+
+
+public class CircleCalculator {
+
+    public static final double PI = 3.14;
+
+
+    public double calculatePerimeter(double r) {
+        return 2 * PI * r;
+    }
+
+    public double calculateArea(double r) {
+        return PI * r * r;
+
+    }
+}
