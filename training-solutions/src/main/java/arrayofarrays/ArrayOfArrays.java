d1eae1a32abdd0cb1cbe156b070ab9104b4ae944
anitaszoke <szanita1735@gmail.com>
2021-10-27 07:48:28 +0200

    Tömbök tömbjének kiírása task done

--- training-solutions/src/main/java/arrayofarrays/ArrayOfArrays.java
+++ training-solutions/src/main/java/arrayofarrays/ArrayOfArrays.java
@@ -3,8 +3,20 @@ package arrayofarrays;
 public class ArrayOfArrays {
 
         public void printArrayOfArrays(int[][] a) {
+            for (int itemA[] : a) {
+                for (int itemAoA : itemA)
+                System.out.print(itemAoA);
+                System.out.println(" ");
+            }
+        }
 
-
+    public static void main(String[] args) {
+        ArrayOfArrays arrayOfArrays = new ArrayOfArrays();
+        int[][] arrays = {{0, 1, 2}, {0, 1, 2}, {0, 1, 2}, {0, 1, 2}};
+        arrayOfArrays.printArrayOfArrays(arrays);
 
     }
+
+
+
 }
