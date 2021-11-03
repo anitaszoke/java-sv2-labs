19377acca07cff4800e7fb761eeceb39b34adb54
anitaszoke <szanita1735@gmail.com>
2021-11-03 17:37:22 +0100

    Folyamatban

---
+++ training-solutions/src/main/java/stringbuilder/airport/Airport.java
@@ -0,0 +1,16 @@
+package stringbuilder.airport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Airport {
+    private List<Flight> flights = new ArrayList<>();
+
+    public void addFlight(Flight flight) {
+        flights.add(flight);
+    }
+
+    public String getDeletedFlights() {
+        if
+    }
+}
