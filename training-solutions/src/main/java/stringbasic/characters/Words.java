04451028e3845e1020bd2ca56de140163c52d104
anitaszoke <szanita1735@gmail.com>
2021-11-05 14:28:02 +0100

    String alapok Online oktatás All done

--- training-solutions/src/main/java/stringbasic/characters/Words.java
+++ training-solutions/src/main/java/stringbasic/characters/Words.java
@@ -1,8 +1,38 @@
 package stringbasic.characters;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Words {
 
+    public void wordLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy szót");
+        String word = scanner.nextLine();
+        System.out.println("Kérem betűzze le a következő szót: " + word);
 
+        char[] letterList = new char[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            String letter = scanner.nextLine();
+            letterList[i] = letter.toCharArray()[0];
+        }
+        System.out.println("A megadott szó: " + word);
+        String letterToLetter = Arrays.toString(letterList);
+        System.out.println("A betűk: " + letterToLetter);
+        String letterToWord = String.valueOf(letterList);
+        System.out.println("A betűzött szó: " + letterToWord);
+
+        if (word.equals(letterToWord)) {
+            System.out.println("Helyes a megoldás");
+        } else {
+            System.out.println("Helytelen a megoldás");
+        }
     }
+
+    public static void main(String[] args) {
+        Words words = new Words();
+        words.wordLetter();
+    }
+}
