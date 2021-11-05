a99fa0a54eb78b4ea3ed23265a61a8469234d3ea
anitaszoke <szanita1735@gmail.com>
2021-11-05 14:48:31 +0100

    Stringbuilder - Törölt járatok All done

--- training-solutions/src/main/java/stringbuilder/airport/Airport.java
+++ training-solutions/src/main/java/stringbuilder/airport/Airport.java
@@ -11,6 +11,25 @@ public class Airport {
     }
 
     public String getDeletedFlights() {
-        if
+        System.out.println("Törölt járatok: ");
+        for (int i = 0; i < flights.size(); i++) {
+            if (flights.get(i).getStatus() == Status.DELETED) {
+                System.out.println(flights.get(i).getFlightNumber());
+
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Airport airport = new Airport();
+        Flight flight = new Flight("AB-945", Status.ACTIVE);
+        Flight flight1 = new Flight("ABC-945", Status.DELETED);
+        Flight flight2 = new Flight("ABCD-945", Status.DELETED);
+        airport.addFlight(flight);
+        airport.addFlight(flight1);
+        airport.addFlight(flight2);
+
+        airport.getDeletedFlights();
     }
 }
