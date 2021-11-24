aaefb0ee36267429d8824e5f7186ef72a2d1db57
anitaszoke <szanita1735@gmail.com>
2021-11-24 01:46:54 +0100

    9_Fájl beolvasása - Bevásárlólista

---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,18 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+    public static void main(String[] args) {
+        try {
+            List<String> items = Files.readAllLines(Paths.get("shoppinglist.txt"));
+            System.out.println(items);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("Can not read file");
+        }
+    }
+}
