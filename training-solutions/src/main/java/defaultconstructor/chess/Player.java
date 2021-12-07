297ae2183f5823b23519127b0b64bdabd071590d
anitaszoke <szanita1735@gmail.com>
2021-12-07 23:52:28 +0100

    13 - 01 - Default és paraméter nélküli konstruktor - Sakkverseny

---
+++ training-solutions/src/main/java/defaultconstructor/chess/Player.java
@@ -0,0 +1,31 @@
+package defaultconstructor.chess;
+
+public class Player {
+    private String name;
+    private String mail;
+    private String sign;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
