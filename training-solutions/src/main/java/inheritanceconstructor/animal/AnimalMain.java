0d254c9c1ed0c27eff68577dd329228a90646b5b
anitaszoke <szanita1735@gmail.com>
2021-12-09 00:48:38 +0100

    14 - 02 - Konstruktorok és az öröklődés viszonya - Konstruktorok hívási lánca

---
+++ training-solutions/src/main/java/inheritanceconstructor/animal/AnimalMain.java
@@ -0,0 +1,12 @@
+package inheritanceconstructor.animal;
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+        Sparrow sparrow = new Sparrow();
+
+        //Animal class constructor
+        //Bird class constructor
+        //Sparrow class constructor
+    }
+}
