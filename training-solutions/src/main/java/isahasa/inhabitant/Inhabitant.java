15feaaf4f6606d8a0e0404130a5fa3efc751c143
anitaszoke <szanita1735@gmail.com>
2022-01-04 19:16:33 +0100

    16_3 - is-a-has-a - A lakók parkolóhelyei

---
+++ training-solutions/src/main/java/isahasa/inhabitant/Inhabitant.java
@@ -0,0 +1,20 @@
+package isahasa.inhabitant;
+
+public class Inhabitant extends Person {
+
+    private ParkingPlace parkingPlace;
+
+    public Inhabitant(String name, ParkingPlace parkingPlace) {
+        super(name);
+        this.parkingPlace = parkingPlace;
+    }
+
+    public ParkingPlace getParkingPlace() {
+        return parkingPlace;
+    }
+
+    public void changeParkingPlace(int numberNew) {
+        ParkingPlace parkingPlaceNew = new ParkingPlace(numberNew);
+        this.parkingPlace = parkingPlaceNew;
+    }
+}
