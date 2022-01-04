576bab27aab7f4172300a8d47f866b50e4e0eb09
anitaszoke <szanita1735@gmail.com>
2022-01-04 18:12:38 +0100

    15_8 - Absztrakt metódusok implementálása felsorolásos típusokban - Kedvezmények

---
+++ training-solutions/src/test/java/enumabstract/DiscountTest.java
@@ -0,0 +1,25 @@
+package enumabstract;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountTest {
+
+    @Test
+    void specialOfferTest() {
+        Discount discount = Discount.SPECIAL_OFFER;
+
+        assertEquals(80, discount.getAmountToPay(10, 10));
+    }
+
+    @Test
+    void blackFridayOfferTest() {
+        Discount discount = Discount.BLACK_FRIDAY_OFFER;
+
+        assertEquals(15, discount.getAmountToPay(10, 2));
+        assertEquals(20, discount.getAmountToPay(10, 3));
+        assertEquals(35, discount.getAmountToPay(10, 5));
+    }
+
+}
